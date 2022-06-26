6ba9e645448669921975f5c851d24469dfef55ce
Carlos José González Juan <65392432+Carlosj50@users.noreply.github.com>
2022-06-26 05:40:34 +0100

    manejo de string

---
+++ manejostring.py
@@ -0,0 +1,9 @@
+mi_string = "Hola mundo" # String
+
+print (len(mi_string)) # Longitud de la cadena
+
+mi_lista = ['carlos', 'juan', 'pedro'] # lista de strings
+print (mi_lista[0]) # carlos
+primer_nombre = mi_lista[0] # carlos
+print (primer_nombre) # carlos
+print (primer_nombre[0])    # Imprime el primer caracter de la primera palabra
